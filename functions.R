64ef6bb244de0725a29eabf432e80627b4bd5f88
whirlsyu <42082429+whirlsyu@users.noreply.github.com>
2020-12-20 02:06:12 +0800

    Update functions.R

--- functions.R
+++ functions.R
@@ -129,50 +129,49 @@ auc_cal_cv_tvt <- function(single_one,
            auc_COX_test,auc_SVR_test,auc_eNet_test,auc_em_test))
 }
 
+auc_cal_cv_tt <- function(single_one,all_data_train,data_test,y_surv_train,y_surv_test,nfold=10,seed=NULL) {
+  # em<-ensemble_model(t(single_one),all_data_combat[,c(grep('TCGA',colnames(all_data_combat)),grep('GSM',colnames(all_data_combat))[1:50])],
+  #                    y_surv_train)
+  set.seed(seed)
+  sp<-sample(1:length(y_surv_train),replace = F)
+  order_sp<-order(sp)
+  y_surv_train = y_surv_train[sp]
+  all_data_train = all_data_train[,sp]
+  
+  train<-1:length(y_surv_train)
+  folds <- cut(seq(1,length(train)),breaks=nfold,labels=FALSE)
+  survival_predictions<-NULL
+  for (i in seq(unique(folds))) {
+    rz<- which(folds==i,arr.ind=TRUE)
+    em<-ensemble_model(t(single_one),all_data_train[,train[-rz]],y_surv_train[-rz])
+    survival_prediction<-ensemble_prediction(em,all_data_train[,train[rz]])
+    survival_predictions<-cbind(survival_predictions,survival_prediction)
+  }
+  
+  em_all<-ensemble_model(t(single_one),all_data_train,y_surv_train)
+  pre_test<-ensemble_prediction(em_all,data_test)
+
+  auc_COX_test<-auc_roc(y_surv = y_surv_test,pre_test['cox',])
+  auc_SVR_test<-auc_roc(y_surv = y_surv_test,pre_test['svm',])
+  auc_eNet_test<-auc_roc(y_surv = y_surv_test,pre_test['lasso',])
+  auc_em_test<-auc_roc(y_surv = y_surv_test,pre_test['ensemble',])
+
+  auc_COX_cv<-auc_roc(y_surv = y_surv_train,survival_predictions['cox',])
+  auc_SVR_cv<-auc_roc(y_surv = y_surv_train,survival_predictions['svm',])
+  auc_eNet_cv<-auc_roc(y_surv = y_surv_train,survival_predictions['lasso',])
+  auc_em_cv<-auc_roc(y_surv = y_surv_train,survival_predictions['ensemble',])
+  
+  return(c(single_one,
+           auc_COX_cv,auc_SVR_cv,auc_eNet_cv,auc_em_cv,
+           auc_COX_test,auc_SVR_test,auc_eNet_test,auc_em_test))
+}
+
 auc_roc <- function(y_surv,predictions) {
   survivalROC::survivalROC.C(Stime = y_surv[,1],
                            status = y_surv[,2],
                            marker = predictions,
                            predict.time = 5,
                            span =0.25*length(y_surv[,1])^(-0.20))$AUC
-}
-
-ROC_multiple_clinical<-function(test_frame,y_surv,file_name="title",ntime=5){
-  requireNamespace("prognosticROC") #version 0.7
-  sroclong_all<-NULL
-  for (n in 1:ncol(test_frame)) {
-    ROC_res= survivalROC::survivalROC(Stime=y_surv[,1],
-                                      status=y_surv[,2],
-                                      marker =as.numeric(test_frame[,n]),
-                                      lambda = NULL,
-                                      predict.time = ntime,method = "NNE",span =0.25*length(y_surv[,1])^(-0.20))#
-    sroclong_all<-ROCdata_save(sroclong_all,ROC_res,mark = paste(ntime,"year AUC at",colnames(test_frame)[n],round(ROC_res$AUC,2),collapse = " "))
-  }
-  # The palette with grey:
-  cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-  # The palette with black:
-  cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-  # To use for fills, add
-  # scale_fill_manual(values=cbPalette)
-  # To use for line and point colors, add
-  gg<-ggplot(sroclong_all, aes(x = FPF, y = TPF, label = c, color = group))+
-    coord_cartesian(ylim=c(0, 1.05),xlim=c(0, 1.05),expand=FALSE)+
-    geom_roc(labels = FALSE, stat = "identity") + style_roc(theme = theme_light())+
-    theme(panel.grid.major =element_blank(), panel.grid.minor = element_blank(),
-          panel.background = element_blank(),axis.line = element_line(colour = "black"))+
-    theme(legend.position=c(0.8,0.3), legend.justification=c(1,1),legend.title = element_blank())+
-    geom_abline(slope=1, colour="black")+scale_colour_manual(values=cbPalette)
-  ggsave(filename = paste("Time ROC of ",file_name,".jpeg",sep=""),plot = gg,device ="jpeg" ,
-         path = getwd(),dpi = 300,units = "in",width = 5, height = 4.5,
-         limitsize=F)
-}
-
-ROCdata_save<-function(origin=NULL,perf,mark="none"){
-  sroclong<-data.frame(TPF = perf$TP, FPF =perf$FP, 
-                       c = perf$cut.values, 
-                       group = rep(mark, length(perf$TP)) )
-  sroclong<-rbind(origin,sroclong)
-  sroclong
 }
 
 ensemble_prediction.m<- function(ensemble_model,predition_data) {

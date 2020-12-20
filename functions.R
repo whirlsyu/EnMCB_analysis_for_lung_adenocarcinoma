ce53b41fb7ab945583f79e86b09318fd8fe627d2
whirlsyu <42082429+whirlsyu@users.noreply.github.com>
2020-12-21 00:22:14 +0800

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,23 +1,31 @@
 # tested environment:
-# platform       x86_64-redhat-linux-gnu     
-# arch           x86_64                      
-# os             linux-gnu                   
-# system         x86_64, linux-gnu           
-# status                                     
-# major          3                           
-# minor          5.0                         
-# year           2018                        
-# month          04                          
-# day            23                          
-# svn rev        74626                       
-# language       R                           
-# version.string R version 3.5.0 (2018-04-23)
-# nickname       Joy in Playing   
-# those functions are also compatible with 4.0.3
-# source other function
+# those functions are compatible with 3.6.0 or higher
 
 require('survivalROC')
 require('prognosticROC')
+
+get_the_datasets<-function(type){
+  if (!file.exists('annotation_met.txt')) download.file('ftp://server.genelibs.com/en_mcb/annotation_met.txt',destfile = 'annotation_met.txt')      #download the annotation data
+  if (type == 'training/testing'){
+    if (!file.exists('training_testing_data.RData')){
+      #Considering the data size up to 1.7Gb, the data downloading could be a time consuming process.
+      #download the data sets
+      download.file('ftp://server.genelibs.com/en_mcb/training_testing_precedure/training_testing_data.RData',
+                    destfile = 'training_testing_data.RData' )
+    }
+    load('training_testing_data.RData',envir = globalenv())
+  }else if (type == 'training/validation/testing'){
+    if (!file.exists('training_validation_testing_data.RData')){
+      #Considering the data size up to 1.7Gb, the data downloading could be a time consuming process.
+      #download the data sets
+      download.file('ftp://server.genelibs.com/en_mcb/training_testing_precedure/training_validation_testing_data.RData',
+                    destfile = 'training_validation_testing_data.RData' )
+    }
+    load('training_validation_testing_data.RData',envir = globalenv())
+  }else{
+    cat("unable to recognize the type code, please check it.")
+  }
+}
 
 auc_cal_cv<- function(single_one,train_set,y_surv_train,nfold=10,seed=NULL) {
     library(survival)
@@ -45,7 +53,7 @@ auc_cal_cv_tvt <- function(single_one,
                            train_set,validation_set,test_set,
                            y_surv_train,y_surv_validation,y_surv_test,
                            seed=9){
-  library(survival)
+  require(survival)
   
   #remove all the NA and zero survival data
   na_or_zero_data<-(is.na(y_surv_train[,1])|y_surv_train[,1]==0)
@@ -129,9 +137,17 @@ auc_cal_cv_tvt <- function(single_one,
            auc_COX_test,auc_SVR_test,auc_eNet_test,auc_em_test))
 }
 
+
 auc_cal_cv_tt <- function(single_one,all_data_train,data_test,y_surv_train,y_surv_test,nfold=10,seed=NULL) {
-  # em<-ensemble_model(t(single_one),all_data_combat[,c(grep('TCGA',colnames(all_data_combat)),grep('GSM',colnames(all_data_combat))[1:50])],
-  #                    y_surv_train)
+  #remove all the NA and zero survival data
+  na_or_zero_data<-(is.na(y_surv_train[,1])|y_surv_train[,1]==0)
+  all_data_train = all_data_train[,!na_or_zero_data]
+  y_surv_train = y_surv_train[!na_or_zero_data]
+  
+  na_or_zero_data<-(is.na(y_surv_test[,1])|y_surv_test[,1]==0)
+  data_test = data_test[,!na_or_zero_data]
+  y_surv_test = y_surv_test[!na_or_zero_data]
+  
   set.seed(seed)
   sp<-sample(1:length(y_surv_train),replace = F)
   order_sp<-order(sp)
@@ -144,21 +160,21 @@ auc_cal_cv_tt <- function(single_one,all_data_train,data_test,y_surv_train,y_sur
   for (i in seq(unique(folds))) {
     rz<- which(folds==i,arr.ind=TRUE)
     em<-ensemble_model(t(single_one),all_data_train[,train[-rz]],y_surv_train[-rz])
-    survival_prediction<-ensemble_prediction(em,all_data_train[,train[rz]])
+    survival_prediction<-ensemble_prediction(em,all_data_train[,train[rz]],mutiple_results = T)
     survival_predictions<-cbind(survival_predictions,survival_prediction)
   }
   
   em_all<-ensemble_model(t(single_one),all_data_train,y_surv_train)
-  pre_test<-ensemble_prediction(em_all,data_test)
-
+  pre_test<-ensemble_prediction(em_all,data_test,mutiple_results = T)
+  
   auc_COX_test<-auc_roc(y_surv = y_surv_test,pre_test['cox',])
   auc_SVR_test<-auc_roc(y_surv = y_surv_test,pre_test['svm',])
-  auc_eNet_test<-auc_roc(y_surv = y_surv_test,pre_test['lasso',])
+  auc_eNet_test<-auc_roc(y_surv = y_surv_test,pre_test['enet',])
   auc_em_test<-auc_roc(y_surv = y_surv_test,pre_test['ensemble',])
-
+  
   auc_COX_cv<-auc_roc(y_surv = y_surv_train,survival_predictions['cox',])
   auc_SVR_cv<-auc_roc(y_surv = y_surv_train,survival_predictions['svm',])
-  auc_eNet_cv<-auc_roc(y_surv = y_surv_train,survival_predictions['lasso',])
+  auc_eNet_cv<-auc_roc(y_surv = y_surv_train,survival_predictions['enet',])
   auc_em_cv<-auc_roc(y_surv = y_surv_train,survival_predictions['ensemble',])
   
   return(c(single_one,
@@ -174,22 +190,52 @@ auc_roc <- function(y_surv,predictions) {
                            span =0.25*length(y_surv[,1])^(-0.20))$AUC
 }
 
-ensemble_prediction.m<- function(ensemble_model,predition_data) {
-  predition_data<-predition_data[rownames(predition_data) %in% names(coef(ensemble_model$cox$cox_model)),]
-  if (nrow(predition_data)!=length(rownames(predition_data))) {
-    stop("ERROR: The predition data and the model have wrong dimensions!")
+#'@title ROC calculation for multiple clinical data
+#'@author Xin Yu
+#'@description Demo matrix for methylation matrix.
+#'@param test_frame clinical data as data.frame
+#'@param y_surv Surv object created by survival package
+#'@param file_name the file name used for saving the results
+#'@param ntime time point for time dependent ROC (year).
+#'@return This function will only used for AUC calculation.
+#'@examples 
+
+ROC_multiple_clinical<-function(test_frame,y_surv,file_name="title",ntime=5){
+  require("prognosticROC") #version 0.7
+  require("ggplot2")
+  require("plotROC")
+  sroclong_all<-NULL
+  for (n in 1:ncol(test_frame)) {
+    ROC_res= survivalROC::survivalROC(Stime=y_surv[,1],
+                                      status=y_surv[,2],
+                                      marker =as.numeric(test_frame[,n]),
+                                      lambda = NULL,
+                                      predict.time = ntime,method = "NNE",span =0.25*length(y_surv)^(-0.20))#
+    sroclong_all<-ROCdata_save(sroclong_all,ROC_res,mark = paste(ntime,"year AUC at",colnames(test_frame)[n],round(ROC_res$AUC,2),collapse = " "))
   }
-  svm<-stats::predict(ensemble_model$svm$svm_model, data.frame(t(predition_data)))$predicted
-  cox<-stats::predict(ensemble_model$cox$cox_model, data.frame(t(predition_data)))
-  enet<-stats::predict(ensemble_model$enet$`enet model`,t(predition_data),s=ensemble_model$enet$`corrected lambda(min)`)
-  data<-rbind(cox,
-              svm,
-              t(enet)
-  )
-  rownames(data)<-c('cox','svm','enet')
-  data<-t(data)
-  data_f<-as.data.frame(data)
-  ensemble=stats::predict(ensemble_model$stacking, data_f)
-  return(t(cbind(data,ensemble)))
+  # The palette with grey:
+  cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  # The palette with black:
+  # cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  # To use for fills, add
+  #scale_fill_manual(values=cbPalette)
+  # To use for line and point colors, add
+  gg<-ggplot2::ggplot(sroclong_all, aes(x = FPF, y = TPF, label = c, color = group))+
+    coord_cartesian(ylim=c(0, 1.05),xlim=c(0, 1.05),expand=FALSE)+
+    geom_roc(labels = FALSE, stat = "identity") + style_roc(theme = theme_light())+
+    theme(panel.grid.major =element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(),axis.line = element_line(colour = "black"))+
+    theme(legend.position=c(0.8,0.3), legend.justification=c(1,1),legend.title = element_blank())+
+    geom_abline(slope=1, colour="black")+scale_colour_manual(values=cbPalette)
+  ggplot2::ggsave(filename = paste("Time ROC of ",file_name,".jpeg",sep=""),plot = gg,device ="jpeg" ,
+         path = getwd(),dpi = 300,units = "in",width = 5, height = 4.5,
+         limitsize=F)
 }
 
+ROCdata_save<-function(origin=NULL,perf,mark="none"){
+  sroclong<-data.frame(TPF = perf$TP, FPF =perf$FP, 
+                       c = perf$cut.values, 
+                       group = rep(mark, length(perf$TP)) )
+  sroclong<-rbind(origin,sroclong)
+  sroclong
+}
